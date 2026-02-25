381158361074d3db64965306258e0393c9a2f76a
Anatol <seddboom@gmail.com>
2026-02-25 14:02:13 +0300

    Update recursive_cte.sql

--- tasks/recursive_cte.sql
+++ tasks/recursive_cte.sql
@@ -13,3 +13,47 @@ INSERT INTO employees VALUES
   (3, 1, "Carol"),
   (4, 2, "Dave"),
   (5, 2, "Eve");
+
+--Recursive CTEs are common table expressions defined with the RECURSIVE keyword. They consist of two parts combined
+--by using UNION ALL:
+WITH RECURSIVE employee_hierarchy AS (
+    -- This runs once and seeds the recursion
+    SELECT employee_id,
+      manager_id,
+      "" as manager_name,
+      name,
+      1 AS level
+    FROM
+      employees
+    WHERE
+      manager_id IS NULL  -- Start from top manager (Alice)
+
+    UNION ALL
+
+    -- This refers to the CTE itself and is repeatedly applied to build new rows.
+    -- This continues until no new rows are produced.
+    SELECT
+      e.employee_id,
+      e.manager_id,
+      eh.name,
+      e.name,
+      eh.level + 1
+    FROM
+      employees e
+    INNER JOIN employee_hierarchy eh ON e.manager_id = eh.employee_id
+)
+SELECT
+  *
+FROM
+  employee_hierarchy
+ORDER BY
+  level, employee_id;
+--+-------------+-----------+--------------+------+--------+
+--|  employee_id| manager_id|  manager_name|  name|   level|
+--+-------------+-----------+--------------+------+--------+
+--|            1|       null|              | Alice|       1|
+--|            2|          1|         Alice|   Bob|       2|
+--|            3|          1|         Alice| Carol|       2|
+--|            4|          2|           Bob|  Dave|       3|
+--|            5|          2|           Bob|   Eve|       3|
+--+-------------+-----------+--------------+------+--------+
